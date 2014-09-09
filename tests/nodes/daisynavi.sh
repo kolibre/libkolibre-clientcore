ca1b8dc89556b9e27dda87af34cf04e969284c54
Johan Abbors <johan@pratsam.org>
2014-09-09 10:41:33 +0300

    Rename datafolder to testdata

--- tests/nodes/daisynavi.sh
+++ tests/nodes/daisynavi.sh
@@ -4,4 +4,4 @@ if [ -x /usr/bin/gdb ]; then
     PREFIX="libtool --mode=execute gdb --batch --return-child-result -x ${srcdir:-.}/run --args"
 fi
 
-${PREFIX} ${bindir:-.}/daisynavi ${srcdir:-.}/daisyNavi/FireSafety/ncc.html
+${PREFIX} ${bindir:-.}/daisynavi ${srcdir:-.}/testdata/FireSafety/ncc.html
